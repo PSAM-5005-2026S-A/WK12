e30d88ad1f072f5cb438cfc98973f48940c418d3
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-04-28 01:07:57 +0000

    added graffiti data and drawing

--- www/graffiti/sketch.js
+++ www/graffiti/sketch.js
@@ -1,7 +1,8 @@
 async function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth-1, windowHeight-1);
   const res = await fetch("../data/graffiti.json");
   const data = await res.json();
+  // console.log(data);
 
   const dataByLon = data.toSorted((a, b) => a["longitude"] - b["longitude"]);
   dataByLon.unshift(dataByLon[0]);
@@ -15,16 +16,31 @@ async function setup() {
     lats.push(dataByLon[i]["latitude"]);
     lons.push(dataByLon[i]["longitude"]);
   }
+  console.log(lats);
+  console.log(lons);
   drawGraffiti(lats, lons);
 }
 
 function drawGraffiti(lats, lons) {
   background(222);
-  strokeWeight(4);
+  strokeWeight(2);
 
   // TODO: get mins and maxs
+  const minLon = min(lons);
+  const maxLon = max(lons);
+  const minLat = min(lats);
+  const maxLat = max(lats);
 
   // TODO: draw a visualization relating graffiti locations
+  beginShape();
   for (let i = 0; i < lats.length; i++) {
+    const x = map(lons[i], minLon, maxLon, 0, width);
+    const y = map(lats[i], minLat, maxLat, height, 0);
+
+    fill(0, 0, 0);
+    ellipse(x, y, 4);
+    curveVertex(x,y);
   }
+  noFill();
+  endShape(OPEN);
 }
